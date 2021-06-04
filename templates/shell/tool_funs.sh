49150fe9e168470f9f9a4bb20d5d9f199ebef348
pengji <jipengpro@gmail.com>
2021-06-04 14:41:13 +0800

    add templates

---
+++ templates/shell/tool_funs.sh
@@ -0,0 +1,66 @@
+# function
+function get_network_config {
+    echo "sysconfig"
+    return
+}
+# call function
+$(get_network_config)
+
+
+# check whether shell command is successful
+function check_cmd {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $@" >&2
+        exit $status
+    fi
+}
+
+
+# parse yaml file
+function parse_yaml {
+    local yaml_file=$1
+    local parse_conf_result=$2
+    local s
+    local w
+    local fs
+
+    s='[[:space:]]*'
+    w='[a-zA-Z0-9_.-]*'
+    fs="$(echo @|tr @ '\034')"
+
+    (
+        sed -e '/- [^\�~@~\]'"[^\']"'.*: /s|\([ ]*\)- \([[:space:]]*\)|\1-\'$'\n''  \1\2|g' |
+
+        sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/[[:space:]]*$//g;' \
+            -e 's/\$/\\\$/g' \
+            -e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;" \
+            -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+            -e "s|^\($s\)\($w\)${s}[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
+
+        awk -F"$fs" '{
+            indent = length($1)/2;
+            # if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+            vname[indent] = $2;
+            for (i in vname) {if (i > indent) {delete vname[i]}}
+                if (length($3) > 0) {
+                    vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])(";")}
+                    print vn, $2, $3 > "'$parse_conf_result'"
+                }
+            }' |
+
+        sed -e 's/_=/+=/g' |
+
+        awk 'BEGIN {
+                FS="=";
+                OFS="="
+            }
+            /(-|\.).*=/ {
+                gsub("-|\\.", "_", $1)
+            }
+            { print }'
+    ) < "$yaml_file"
+}
+# usage
+parse_yaml source.yaml parse_result
