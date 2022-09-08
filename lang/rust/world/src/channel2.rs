cb80aff54acab468778942875cb51d7a3a1f516d
pengji <jipengpro@gmail.com>
2022-09-08 19:54:09 +0800

    add rust part2

---
+++ lang/rust/world/src/channel2.rs
@@ -0,0 +1,15 @@
+// 多个生产者单个消费者
+use std::thread;
+use std::sync::mpse;
+
+fn fake_main() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // println!("{}", val);  // 调用 send 的时候，会发生 move，此处不能再使用 val
+    });
+
+    let re = rx.recv().unwrap();
+    println!("Got {}", re);
+}
